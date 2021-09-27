75a90205540da3f7244f05a62e167077520b0a4d
Omer Segev <omersegev@aidoc.com>
2021-09-27 18:10:27 +0300

    Upgrade pandas-data reader to 0.10.0

--- tests/test_get_stock.py
+++ tests/test_get_stock.py
@@ -1,3 +1,8 @@
+STOCK = "FB"
+START = 2020
+END = 2021
+
+
 def test_import(import_lib):
     data = import_lib
     assert data
@@ -5,5 +10,5 @@ def test_import(import_lib):
 
 def test_get_stock_dataframe_shape(import_lib):
     data = import_lib
-    df = data.get_stock("A", "2020", "2021")
+    df = data.get_stock(STOCK, START, END)
     assert df.shape == (253, 16)
