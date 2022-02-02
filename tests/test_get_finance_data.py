6413392b05cd51e50e52aa49b98a7dde2e4099c1
Omer Segev <omersegev@gmail.com>
2022-02-02 08:28:23 +0200

    Fixed FRED finance data test

--- tests/test_get_finance_data.py
+++ tests/test_get_finance_data.py
@@ -6,4 +6,4 @@ def test_import(import_lib):
 def test_get_stock_dataframe_shape(import_lib):
     data = import_lib
     df = data.get_finance_data("2020", "2021")
-    assert df.shape == (267, 17)
+    assert df.shape == (267, 16)
