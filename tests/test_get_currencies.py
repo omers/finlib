10f8f65c07791b84b398ef3deb6ae14d6caa27e6
Omer Segev <omersegev@gmail.com>
2022-02-19 10:36:03 +0200

    Added Yahoo finance module + Upgraded pandas + numpy to the latest

--- tests/test_get_currencies.py
+++ tests/test_get_currencies.py
@@ -6,4 +6,4 @@ def test_import(import_lib):
 def test_get_currencies_dataframe_shape(import_lib):
     data = import_lib
     df = data.get_currencies("2020", "2021")
-    assert df.shape == (263, 7)
+    assert df.shape == (263, 6)
