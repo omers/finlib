f95976772a2954a9054bd6e2f8f9cacead2c5aaf
Omer Segev <omersegev@gmail.com>
2022-11-09 10:44:31 +0200

    Removed yfinance and update pandas + numpy

--- src/finpredict/lib.py
+++ src/finpredict/lib.py
@@ -1,9 +1,7 @@
 import pandas_datareader.data as web
 from pandas_datareader import wb
 import pandas as pd
-import numpy as np
 import requests
-import yfinance as yf
 import math, decimal, datetime
 
 USER_AGENT = {
